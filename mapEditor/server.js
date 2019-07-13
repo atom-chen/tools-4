6672b5f79cd18ba373bd8279dcc943815a5b347e
zhangzhiqiang <youxi123>
2019-07-13 16:18:55 +0800

    修改递边工具

--- mapEditor/server.js
+++ mapEditor/server.js
@@ -10,7 +10,9 @@ app.use('/', express.static(__dirname + '/www'));
 //bind the server to the 80 port
 //server.listen(3000);//for local test
 server.listen(process.env.PORT || 3000);//
-console.log('请在浏览器输入---http://本机ip:3000 or http://localhost:3000')
+// console.log('请在浏览器输入---http://本机ip:3000 or http://localhost:3000')
+
+exec(`open -a "/Applications/Google Chrome.app" "http://localhost:3000"`)
 //server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
 //console.log('server started on port'+process.env.PORT || 3000);
 //handle the socket

a401b49c47db6079aa1134d2a24db8eee054801f
zhangzhiqiang <youxi123>
2019-09-16 18:01:31 +0800

    修改web【

--- webp2png/webp2png.js
+++ webp2png/webp2png.js
@@ -4,6 +4,8 @@ let fs = require('fs');
 const glob = require("glob");
 const param = process.argv.slice(2)[0];
 
+var webp=require('webp-converter');
+
 function getExn(dir) {
 	if(!dir) {
 		return false;
@@ -20,8 +22,11 @@ glob("resource/**/*.*", function(er, files) {
 	files.forEach(function(v, i) {
 		var types = getExn(v);
 			if(Exname.includes(types)) {
-                let newPath = "transResource/"+v.substring(v.lastIndexOf("/")+1)
-                exec(`dwebp ${v} -o ${newPath}`)
+                let newPath = "transResource/"+v.substring(v.lastIndexOf("/")+1);
+				webp.dwebp(`${v}`,`${newPath}`,"-o",function(status,error){
+					console.log("解析成功----"+`${newPath}`)
+				})
+                // exec(`dwebp ${v} -o ${newPath}`)
                 // exec(`cwebp -q 80 ${v} -o ${newPath}`)
 			} 
 	})
